a99cd19b76f90bdb6c6a5b3d9067932cab63acea
rokipdj88 <candraxr4@gmail.com>
2025-01-11 17:36:06 +0700

    Update t3rn.sh

--- t3rn.sh
+++ t3rn.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-screen -S t3rn
 # Logo Teks Menarik
-echo "==============================================="
-echo "  EEEEE   CCCCC   H   H   AAAAA  N   N  EEEEE  L        X   X   CCCCC   AAAAA  N   N  DDDD   RRRR   AAAAA  PPPP   N   N"
-echo "  E       C       H   H   A   A  NN  N  E      L         X X   C       A   A  NN  N  D   D  R   R  A   A  P   P  NN  N"
-echo "  EEEE    C       HHHHH   AAAAA  N N N  EEEE   L         X X   C       AAAAA  N N N  D   D  RRRR   AAAAA  PPPP   N N N"
-echo "  E       C       H   H   A   A  N  NN  E      L         X X   C       A   A  N  NN  D   D  R   R  A   A  P      N  NN"
-echo "  EEEEE   CCCCC   H   H   A   A  N   N  EEEEE  LLLLL     X X   CCCCC   A   A  N   N  DDDD   R   R  A   A  P      N   N"
-echo "==============================================="
-echo ""
+echo "#######################################"
+echo "#                                     #"
+echo "#            M A J I K A Y O         #"
+echo "#                                     #"
+echo "#######################################"
+echo "\nWelcome to the installation script!\n"
 echo "==============================================="
 echo "BELI VPS DI @candrapn"
 echo "==============================================="
