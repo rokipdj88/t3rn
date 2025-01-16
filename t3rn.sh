4bb94b18badc6064bafab6b42375717bfe9facd4
rokipdj88 <candraxr4@gmail.com>
2025-01-16 23:57:45 +0700

    Update t3rn.sh

--- t3rn.sh
+++ t3rn.sh
@@ -18,9 +18,9 @@ sleep 5
 # Menghapus file executor lama jika ada
 echo
 echo -e "Menghapus versi Sebelumnya "
-rm executor-linux-v0.33.0.tar.gz & xecutor-linux-v0.35.0.tar.gz & rm -rf executor
+rm executor-linux-v0.35.0.tar.gz & rm -rf executor
 sleep 3
-echo -e "Mendownload versi terbaru executor-linux-v0.35.0.tar.gz"
+echo -e "Mendownload versi terbaru executor-linux-v0.36.0.tar.gz"
 echo
 mkdir -p t3rn
 cd t3rn
