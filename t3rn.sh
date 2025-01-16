66f6f545a15373cb45b3fb156ef8aa423383da6e
rokipdj88 <candraxr4@gmail.com>
2025-01-16 17:46:06 +0700

    Update t3rn.sh

--- t3rn.sh
+++ t3rn.sh
@@ -16,9 +16,16 @@ echo -e "-----------------------------------------------------"
 sleep 5
 
 # Download file executor baru v 0.33.0
-wget https://github.com/t3rn/executor-release/releases/download/v0.33.0/executor-linux-v0.33.0.tar.gz
+echo 
+echo -e "Menghapus Versi 0.33.0"
+echo
+rm executor-linux-v0.33.0.tar.gz & rm -rf executor
+sleep 3
+echo -e "Mendownload versi terbaru executor-linux-v0.35.0.tar.gz"
+echo
+wget https://github.com/t3rn/executor-release/releases/download/v0.33.0/executor-linux-v0.35.0.tar.gz
 # Ekstrak file
-tar -xzvf executor-linux-v0.33.0.tar.gz
+tar -xzvf executor-linux-v0.35.0.tar.gz
 
 sleep 5
 # Pindah ke direktori executor
