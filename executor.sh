7d9a405092c0c808401327634fa29efcb35f32bf
rokipdj88 <candraxr4@gmail.com>
2025-04-27 10:59:09 +0700

    Update executor.sh

--- executor.sh
+++ executor.sh
@@ -54,7 +54,7 @@ cd $HOME/t3rn
 
 # Input versi
 echo -e "LIST EXECUTOR VERSION HERE : https://github.com/t3rn/executor-release/releases/"
-read -p "Enter spesific version ex: v0.53.1 (press enter for the latest version): " VERSION
+read -p "Enter spesific version ex: v0.70.0 (press enter for the latest version): " VERSION
 if [[ -z "$VERSION" ]]; then
     VERSION=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | \
         grep -Po '"tag_name": "\K.*?(?=")')
@@ -104,7 +104,6 @@ if [[ "$USE_ALCHEMY" == "y" || "$USE_ALCHEMY" == "Y" ]]; then
         "l2rn": ["https://t3rn-b2n.blockpi.network/v1/rpc/public", "https://b2n.rpc.caldera.xyz/http"],
         "arbt": ["https://arb-sepolia.g.alchemy.com/v2/'"$API_ALCHEMY"'"],
         "bast": ["https://base-sepolia.g.alchemy.com/v2/'"$API_ALCHEMY"'"],
-        "blst": ["https://blast-sepolia.g.alchemy.com/v2/'"$API_ALCHEMY"'"],
         "opst": ["https://opt-sepolia.g.alchemy.com/v2/'"$API_ALCHEMY"'"],
         "unit": ["https://unichain-sepolia.g.alchemy.com/v2/'"$API_ALCHEMY"'"],
         "mont": ["https://testnet-rpc.monad.xyz"]
@@ -114,25 +113,23 @@ else
         "l2rn": ["https://t3rn-b2n.blockpi.network/v1/rpc/public", "https://b2n.rpc.caldera.xyz/http"],
         "arbt": ["https://arbitrum-sepolia.drpc.org", "https://sepolia-rollup.arbitrum.io/rpc"],
         "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.drpc.org"],
-        "blst": ["https://sepolia.blast.io", "https://blast-sepolia.drpc.org"],
+        "mont": ["https://testnet-rpc.monad.xyz"],
         "opst": ["https://sepolia.optimism.io", "https://optimism-sepolia.drpc.org"],
-        "unit": ["https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"],
-        "mont": ["https://testnet-rpc.monad.xyz"]
+        "unit": ["https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"]
+}'
     }'
 fi
 
-# Variabel lingkungan lainnya
+
 export ENVIRONMENT=testnet
 export LOG_LEVEL=debug
 export LOG_PRETTY=false
 export EXECUTOR_PROCESS_BIDS_ENABLED=true
 export EXECUTOR_PROCESS_ORDERS_ENABLED=true
 export EXECUTOR_PROCESS_CLAIMS_ENABLED=true
-export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,blast-sepolia,unichain-sepolia,monad-testnet'
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,unichain-sepolia,monad-testnet'
+export EXECUTOR_ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,monad-testnet'
 export PRIVATE_KEY_LOCAL="$PRIVATE_KEY_LOCAL"
-export EXECUTOR_ENABLE_BATCH_BIDING=true
-export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
-export EXECUTOR_PROCESS_ORDERS_API_ENABLED=false
 export EXECUTOR_MAX_L3_GAS_PRICE="$EXECUTOR_MAX_L3_GAS_PRICE"
 
 # Menjalankan dengan screen
